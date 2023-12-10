489d323086c7fd747efd9aa5fc3bdfaa149b84c3
Toru2003 <daniilbal2003@gmail.com>
2023-12-11 00:47:34 +0500

    es6

--- src/es6.js
+++ src/es6.js
@@ -5,25 +5,86 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio) {
+    const [lastName, firstName, middleName] = fio.split(' '); // через сплит добавляем пробел
+
+  // Проверяем, есть ли отчество
+  if (middleName) {
+    return `${firstName} ${lastName}`;
+  } else {
+    return `${firstName} ${lastName}`.trim(); // удаляем пробелы, если они есть(по сути удаляет лишние пробелы)
+  }
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {return Array.from(new Set(array));} //set удаляет повторы, Array.from-возвращает все в массив
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array) {
+      // Проверка на пустой массив
+  if (array.length === 0) {
+    return false;
+  }
+
+  // Находим максимальное и минимальное значение в массиве зарплат
+  const maxSalary = Math.max(...array);
+  const minSalary = Math.min(...array);
+
+  // Проверка, чтобы избежать деления на ноль необезательно но пусть будет
+  if (minSalary === 0) {
+    return 0; 
+  }
+
+  // Вычисляем разницу и возвращаем результат
+  const difference = maxSalary / minSalary;
+  return difference;
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // Словарь - (string, string), и все это не null и не undefined
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    constructor() {
+      this.wordsMap = new Map();
+    }
+  
+    // Добавление слова в словарь
+    addWord(key, value) {
+      // Проверка на null и undefined
+      if (key != null && value != null) {
+        // Проверка, что ключ и значение являются строками
+        if (typeof key === 'string' && typeof value === 'string') {
+          this.wordsMap.set(key, value);
+        } else {
+          throw new Error('Ключ и значение должны быть строками');
+        }
+      } else {
+        throw new Error('Ключ и значение не могут быть null или undefined');
+      }
+    }
+  
+    // Получение значения по ключу
+    getWord(key) {
+      return this.wordsMap.get(key);
+    }
+  
+    // Удаление слова из словаря
+    removeWord(key) {
+      this.wordsMap.delete(key);
+    }
+  
+    // Получение всех слов в словаре
+    getAllWords() {
+      return Array.from(this.wordsMap.keys());
+    }
+  }
 
 module.exports = {
     fioToName,
